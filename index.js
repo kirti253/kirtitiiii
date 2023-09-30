9d9788bf52e708776ed871ad0e5f6910da40fa79
kirti <kirti25032007@gmai.com>
2023-09-30 12:11:52 +0530

    it

---
+++ index.js
@@ -0,0 +1,44 @@
+let firstname ="kirti";
+console.log(firstname);
+
+let javascript="true";
+console.log(javascript);
+
+
+let johnmark = 56;
+let heightjohn = 7;
+let massmark=65;
+let heightmark=6;
+const BMImark=massmark/heightmark;
+const BMIjohn=massmark/heightmark;
+console.log(BMIjohn,BMImark);
+
+
+const number=[
+    24,50, 60,48
+]
+function numberin(arr){
+    let sum = 0;
+    console.log(arr);
+
+    for(let i = 0; i<arr.length; i++){
+        sum = sum+arr[i]
+    }
+    return sum
+}
+
+console.log(numberin(number))
+//basic operators
+
+// const agejohn = 2837-1991;
+//     const agesarah =4793-453;
+// console .log(agejohn,agesarah);
+const now = 3467;
+const agejohn=now-324;
+const agesarah=now-231;
+console.log(agejohn,agesarah);
+
+console.log(agejohn=2,agejohn /10);
+const name='john';
+const lastname='null';
+console.log(name +  lastnameg); 
